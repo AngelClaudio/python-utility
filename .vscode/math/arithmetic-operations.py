a607f461e6fef6ab65596e7bd7be0094c4f531de
Angel Claudio <angelclaudiopr@gmail.com>
2020-07-18 03:23:56 -0400

    up

---
+++ .vscode/math/arithmetic-operations.py
@@ -0,0 +1,13 @@
+# flood division // will divide but not show any decimal places
+print(f"3//5: {3//5} \n3/5: {3/5}")
+print(f"9//5: {9//5} \n9/5: {9/5}")
+
+# Square root can be expressed as exponential calcuations
+print(8 ** (1/3))  # cube root
+print(8 ** (1/2))  # square root
+
+# Function int() will convert numbers to integers
+print(int(3.8))
+
+# Function float() will turn numbers into floats
+print(float(47))
---
+++ .vscode/math/fractions_operations.py
@@ -0,0 +1,5 @@
+# To do operations with fractions you need to import Fraction from fractions
+from fractions import Fraction
+
+f = Fraction(3,4)
+print(f, type(f))
